1e4ab83b69a24b72d6e4fc53426fb68dede273e2
sony <meghanadmore14@gmail.com>
2018-10-31 19:35:18 -0500

    want to commit

---
+++ game/Main.java
@@ -0,0 +1,12 @@
+package game;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int p1score = 0, p2score = 0;
+		Game g = new Game( p1score, p2score );
+		//hey! hi hello
+		g.playGame();
+	}
+
+}
