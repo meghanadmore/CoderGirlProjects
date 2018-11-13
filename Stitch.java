5c648818845bdf7f5d08441f779a3e258523632d
meghanadmore <40777639+meghanadmore@users.noreply.github.com>
2018-11-12 18:06:57 -0600

    Add files via upload

--- Stitch.java
+++ Stitch.java
@@ -9,43 +9,43 @@ public class Stitch {
     public static class ShortLineStitch extends Stitch {
         public void sew() {
             this.stitch = "-";
-            System.out.println(stitch);
+            System.out.print(stitch);
         }
     }
     public static class SmallCrossStitch extends Stitch{
         public void sew(){
             this.stitch="x";
-            System.out.println(stitch);
+            System.out.print(stitch);
         }
     }
     public static class LargeCrossStitch extends Stitch{
         public void sew(){
             this.stitch="X";
-            System.out.println(stitch);
+            System.out.print(stitch);
         }
     }
     public static class CircleStitch extends Stitch{
         public void sew(){
             this.stitch="o";
-            System.out.println(stitch);
+            System.out.print(stitch);
         }
     }
     public static class FillStitch extends Stitch{
         public void sew(){
             this.stitch="=";
-            System.out.println(stitch);
+            System.out.print(stitch);
         }
     }
     public static class BlockStitch extends Stitch{
         public void sew(){
             this.stitch="[]";
-            System.out.println(stitch);
+            System.out.print(stitch);
         }
     }
     public static class ZigZagStitch extends Stitch{
         public void sew(){
             this.stitch="Z";
-            System.out.println(stitch);
+            System.out.print(stitch);
         }
     }
 }
